438b5ac80b3eac05c41e2fbdb2c1b767bf0c8ebc
rieverg <135706205+rieverg@users.noreply.github.com>
2024-05-17 16:47:32 +0300

    Update app_view.py

--- app_view.py
+++ app_view.py
@@ -15,16 +15,7 @@ client = OpenAI(
     api_key='ollama', # required, but unused
 )
 
-response = client.chat.completions.create(
-  model="llama2",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {"role": "assistant", "content": "The LA Dodgers won in 2020."},
-    {"role": "user", "content": "Where was it played?"}
-  ]
-)
-print(response.choices[0].message.content)
+
 
 class AppView:
     def __init__(self, actions):
