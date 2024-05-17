8e1efa8ca62761311cf4954c3aae7700f209ea07
rieverg <135706205+rieverg@users.noreply.github.com>
2024-05-17 16:39:40 +0300

    Update app_view.py

--- app_view.py
+++ app_view.py
@@ -9,10 +9,22 @@ from streamlit_cropper import st_cropper
 from PIL import Image
 import openai
 from openai import OpenAI
+
 client = OpenAI(
     base_url = 'http://localhost:11434/v1',
     api_key='ollama', # required, but unused
 )
+
+response = client.chat.completions.create(
+  model="llama2",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The LA Dodgers won in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+  ]
+)
+print(response.choices[0].message.content)
 
 class AppView:
     def __init__(self, actions):
