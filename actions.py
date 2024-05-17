2dc9d6d274520f87373cc80b30566abe18715710
rieverg <135706205+rieverg@users.noreply.github.com>
2024-05-18 01:05:21 +0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -57,14 +57,14 @@ class Actions:
             )
         
 
-        except openai.APIError as e:
-            st.warning(f"OpenAI API returned an API Error:\n\n{str(e)}\n\n**Refresh the page and try again**")
-            st.session_state["openai_error"] = e
-            st.stop()
-        except openai.APIConnectionError as e:
-            st.warning(f"Failed to connect to OpenAI API:\n\n{str(e)}\n\n**Refresh the page and try again**")
-            st.session_state["openai_error"] = e
-            st.stop()
+        #except openai.APIError as e:
+        #    st.warning(f"OpenAI API returned an API Error:\n\n{str(e)}\n\n**Refresh the page and try again**")
+        #    st.session_state["openai_error"] = e
+         #   st.stop()
+        #except openai.APIConnectionError as e:
+            #st.warning(f"Failed to connect to OpenAI API:\n\n{str(e)}\n\n**Refresh the page and try again**")
+          #  st.session_state["openai_error"] = e
+         #   st.stop()
         except openai.RateLimitError as e:
             st.warning(f"OpenAI API request exceeded rate limit::\n\n{str(e)}\n\n**Fix the problem, refresh the page and try again**")
             st.session_state["openai_error"] = e
